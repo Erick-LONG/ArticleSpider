0d1dbb38b47b9664723ab38b246aefb90699d166
erick <834424581@qq.com>
2018-04-01 18:02:21 +0800

    设置知乎answer页面解析

--- ArticleSpider/spiders/zhihu.py
+++ ArticleSpider/spiders/zhihu.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import re,json
+import re,json,datetime
 from urllib import parse
 from scrapy.loader import ItemLoader
 from ArticleSpider.items import ZhihuAnswerItem,ZhihuQuestionItem
@@ -10,7 +10,8 @@ class ZhihuSpider(scrapy.Spider):
     name = 'zhihu'
     allowed_domains = ['www.zhihu.com']
     start_urls = ['http://www.zhihu.com/']
-
+    #questions的第一页answer的请求URL
+    start_answer_url = 'https://www.zhihu.com/api/v4/questions/{0}/answers?sort_by=default&include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%3F%28type%3Dbest_answerer%29%5D.topics&limit={1}&offset={2}'
     agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
 
     header = {
@@ -32,9 +33,13 @@ class ZhihuSpider(scrapy.Spider):
         for url in all_urls:
             match_obj = re.match('(.*zhihu.com.question/(/d+).*)(/|$)',url)
             if match_obj:
+                #如果提取到question相关页面则下载后交由提取函数进行提取
                 request_url = match_obj.group(1)
                 question_id = match_obj.group(2)
                 yield scrapy.Request(request_url,headers=self.header,callback=self.parse_question)
+            else:
+                #如果不是question页面则进一步跟踪
+                yield scrapy.Request(url,headers=self.header,callback=self.parse)
 
     def parse_question(self,response):
         #处理question页面,并提出具体的question item
@@ -51,7 +56,7 @@ class ZhihuSpider(scrapy.Spider):
             item_loader.add_css('answer_num', '.List-headerText span::text')
             item_loader.add_css('comment_nums', '.QuestionHeader-actions button::text')
             item_loader.add_css('watch_user_num', '.NumberBoard-value::text')
-            item_loader.add_css('topics', '.QuestionHeader-topics .Popover::text')
+            item_loader.add_css('topics', '.QuestionHeader-topics .Popover div::text')
             question_item = item_loader.load_item()
         else:
             #处理旧版本页面item页面提取
@@ -59,16 +64,44 @@ class ZhihuSpider(scrapy.Spider):
             if match_obj:
                 question_id = int(match_obj.group(2))
             item_loader = ItemLoader(item=ZhihuQuestionItem(), response=response)
-            item_loader.add_css('title', '.zh-question-title h2::text')
+            #item_loader.add_css('title', '.zh-question-title h2 a::text')
+            item_loader.add_xpath('title', '//*[@class="zh-question-title"]/h2/span/text()|//*[@class="zh-question-title"]/h2/a/text()')
             item_loader.add_css('content', '#zh-question-detail')
             item_loader.add_value('url', response.url)
             item_loader.add_value('zhihu_id', question_id)
             item_loader.add_css('answer_num', '#zh-question-answer-num::text')
             item_loader.add_css('comment_nums', '#zh-question-meta-wrap a[name="addcomment"]::text')
-            item_loader.add_css('watch_user_num', '.#zh-question-side-header-wrap::text')
+            #item_loader.add_css('watch_user_num', '.#zh-question-side-header-wrap::text')
+            item_loader.add_xpath('watch_user_num', '//*[@id=zh-question-side-header-wrap]/text()|//*[@class="zh-question-fllowers-sidebar"]/div/a/strong/text()')
             item_loader.add_css('topics', '.zm-tag-editor-labels a::text')
             question_item = item_loader.load_item()
 
+        yield scrapy.Request(self.start_answer_url.format(question_id,20,0),headers=self.header,callback=self.parse_answer)
+        yield question_item
+
+    def parse_answer(self,response):
+        #处理question的answer
+        ans_json = json.loads(response.text)
+        is_end = ans_json['paging']['is_end']
+        next_url = ans_json['paging']['next']
+        #提取answer的具体字段
+        for answer in ans_json['data']:
+            answer_item = ZhihuAnswerItem()
+            answer_item['zhihu_id']=answer['id']
+            answer_item['url'] = answer['url']
+            answer_item['question_id'] = answer['question']['id']
+            answer_item['author_id'] = answer['author']['id'] if 'id' in answer['author'] else None
+            answer_item['content'] = answer['content'] if 'content' in answer else None
+            answer_item['parise_num'] = answer['voteup_count']
+            answer_item['comment_nums'] = answer['comment_count']
+            answer_item['create_time'] = answer['created_time']
+            answer_item['update_time'] = answer['updated_time']
+            answer_item['crawl_time'] = datetime.datetime.now()
+
+            yield answer_item
+
+        if not is_end:
+            yield scrapy.Request(next_url,headers=self.header, callback=self.parse_answer)
 
     def start_requests(self):
         return [scrapy.Request('https://www.zhihu.com/#signin',headers=self.header,callback=self.login)]
